e9eda9000ab024808c44192250ca2987b3b96afa
Emran Batmanghelich <emran.bm@gmail.com>
2021-09-03 12:42:03 +0430

    Test: Init acceptance test

---
+++ simulation/acceptance-test/test/main.test.ts
@@ -0,0 +1,25 @@
+import * as assert from 'assert'
+
+describe('save', function () {
+    it('place holder', function () {
+        assert("TODO")
+    });
+});
+
+describe('load', function () {
+    it('place holder', function () {
+        assert("TODO")
+    });
+});
+
+describe('delete', function () {
+    it('place holder', function () {
+        assert("TODO")
+    });
+});
+
+describe('ls', function () {
+    it('place holder', function () {
+        assert("TODO")
+    });
+});
