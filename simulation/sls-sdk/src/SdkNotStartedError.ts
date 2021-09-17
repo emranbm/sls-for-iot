35abcc9015b7f5a3c89712c73985e0730fac9203
Emran <emran.bm@gmail.com>
2021-09-17 22:19:39 +0430

    Feat[sls-sdk]: Sdk not started error message

--- simulation/sls-sdk/src/SdkNotStartedError.ts
+++ simulation/sls-sdk/src/SdkNotStartedError.ts
@@ -1,1 +1,5 @@
-export class SdkNotStartedError extends Error { }
+export class SdkNotStartedError extends Error {
+    constructor(){
+        super("Sdk is not started yet! Try to start it first.")
+    }
+}
