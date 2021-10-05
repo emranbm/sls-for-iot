36a7e6ac58e2cc7bea79a82392dffbdf3b293ab4
Emran <emran.bm@gmail.com>
2021-10-05 16:28:22 +0330

    Test: Add ignore-build option to test script

--- simulation/acceptance-test/run-tests.sh
+++ simulation/acceptance-test/run-tests.sh
@@ -3,9 +3,16 @@
 set -euo pipefail
 
 readonly CWD="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+BUILD=true
 
-cd "$CWD/../"
-./docker-compose-build.sh
+if [ ! -z ${1+x} ] && [ "$1" == "--ignore-build" ]; then
+    BUILD=false
+fi
+
+if $BUILD; then
+    cd "$CWD/../"
+    ./docker-compose-build.sh
+fi
 docker-compose up -d
 cd "$CWD"
-BROKER_URL=mqtt://localhost:1883 ./node_modules/ts-mocha/bin/ts-mocha --exit
+BROKER_URL=mqtt://localhost:1883 ./node_modules/ts-mocha/bin/ts-mocha --exit --timeout 10000
