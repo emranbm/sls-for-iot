86bcaaa57e57293d860598760afa2471af451948
Emran <emran.bm@gmail.com>
2021-10-05 17:02:00 +0330

    Test: Save with single client

--- simulation/acceptance-test/test/main.test.ts
+++ simulation/acceptance-test/test/main.test.ts
@@ -1,17 +1,25 @@
 import * as assert from 'assert'
 import { SlsSdk } from 'sls-sdk'
+import { promises as fs } from 'fs'
 
 const BROKER_URL = process.env.BROKER_URL
+const STORAGE_ROOT = './clients-storages/main/'
+const TEST_CLIENT_ID = "acceptance-test-client"
 
 let sdk: SlsSdk = null
 
 beforeEach(function() {
-    sdk = new SlsSdk(BROKER_URL, "acceptance-test-client")
+    sdk = new SlsSdk(BROKER_URL, TEST_CLIENT_ID, STORAGE_ROOT)
 })
 
 describe('save', function () {
-    it('happy path', async function () {
-        await assert.rejects(sdk.saveFile("some-content", "somefile.txt"))
+    it('can save with single client', async function () {
+        const content = "some-content"
+        const path = "somefile.txt"
+        await sdk.start()
+        await sdk.saveFile(content, path)
+        const savedContent = await fs.readFile(`${STORAGE_ROOT}/${TEST_CLIENT_ID}/${path}`)
+        assert.equal(savedContent.toString(), content)
     });
 });
 
