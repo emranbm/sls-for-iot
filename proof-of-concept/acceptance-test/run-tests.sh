37a9bfa2d982d36b9bfd493f547d391749177fa0
Emran <emran.bm@gmail.com>
2022-05-28 15:06:06 +0430

    Refactor: Move acceptance test compose file inside

--- proof-of-concept/acceptance-test/run-tests.sh
+++ proof-of-concept/acceptance-test/run-tests.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 readonly CWD="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+cd "$CWD"
 docker-compose down
 docker-compose up -d
-cd "$CWD"
 BROKER_URL=mqtt://localhost:1883 ./node_modules/mocha/bin/mocha "$@"
